04f59fbb45ae206dc358cdf3da038d4a967f9962
benwebsta <bmwebster@wisc.edu>
2017-03-16 20:38:06 -0400

    Able to simulate time passing with a startTimer function created with ScheduledExecutorService

--- Week3/TuitionReimbursement/src/com/revature/main/Main.java
+++ Week3/TuitionReimbursement/src/com/revature/main/Main.java
@@ -1,14 +1,34 @@
 package com.revature.main;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import com.revature.service.EmployeeService;
 
 public class Main {
-
+	static int timer = 0;
 	static EmployeeService serveEmp = new EmployeeService();
 	public static void main(String[] args) {
 		
-		System.out.println("benwebsta id: " + serveEmp.loginEmployee("jLee", "password"));	
+		startTimer();
+		System.out.println("benwebsta id: " + serveEmp.loginEmployee("benwebsta", "password"));	
 		
+	}
+	
+	public static void startTimer(){
+	    final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+	    executorService.scheduleAtFixedRate(new Runnable() {
+	        @Override
+	        public void run() {
+	            myTask();
+	            System.out.println(timer);
+	        }
+	    }, 0, 1, TimeUnit.SECONDS);
+	}
+	
+	private static void myTask() {
+	    timer++;
 	}
 
 }
