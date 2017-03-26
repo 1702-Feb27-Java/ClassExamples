50f59859c9dfbf1b460fce33f194855baa5c51d4
benwebsta <bmwebster@wisc.edu>
2017-03-26 16:54:16 -0400

    Dynamic table of pending reimbursements done

--- Week3/TuitionReimbursement/src/servlets/PendingReimbursementsServlet.java
+++ Week3/TuitionReimbursement/src/servlets/PendingReimbursementsServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -39,28 +40,22 @@ public class PendingReimbursementsServlet extends HttpServlet {
 		int deptId = (int) ses.getAttribute("deptId");
 		
 		ArrayList<Reimbursement> reimbursements = new ArrayList<Reimbursement>();
-		if(roleId == 3){
-			//dept head
+		if(roleId == 3){//department head
 			reimbursements = serveEmp.getReimbursements(2, deptId);
 		}
-		else if(roleId == 2){
-			//supervisor
+		else if(roleId == 2){//direct supervisor
 			reimbursements = serveEmp.getReimbursements(1, deptId);
 		}
-		else if(deptId == 1){
-			//benco
+		else if(deptId == 1){//benco
 			reimbursements = serveEmp.getReimbursementsByApprovalStep(3);
 		}
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<h1> empl id : " + empId + "</h1>");
-		out.println("<h1> role id : " + roleId + "</h1>");
-		out.println("<h1> dept id : " + deptId + "</h1>");
-		out.println("<h1> dept id : " + reimbursements + "</h1>");
-		out.println("</body>");
-		out.println("</html>");
+		ses.setAttribute("reimbursements", reimbursements);
+		request.setAttribute("reimbursements", reimbursements);
+		
+		String nextJSP = "/pendingReimbursements.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(nextJSP);
+		dispatcher.forward(request,response);
 	}
 
 	/**
